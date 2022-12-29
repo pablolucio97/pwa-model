67527a921eb32b92bfc95a29d8e044a127bb75b8
pablolucio97 <pablolucio_@hotmail.com>
2022-12-29 07:52:47 -0300

    initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="theme-color" content="#55FF" />
+      </Head>
       <body>
         <Main />
         <NextScript />
